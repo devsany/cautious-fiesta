3306c3bd7b810d6a8a94f57aed2995da767f02df
devsany <sunnyrajbodhgaya19@gmail.com>
2024-12-23 10:05:24 +0530

    add 1 in question 1

--- src/components/in/Input.jsx
+++ src/components/in/Input.jsx
@@ -3,12 +3,20 @@ import React from "react";
 const Input = () => {
   return (
     <div>
+      <h1>Main Form</h1>
+      <h2>Section 1</h2>
+      <span title="myName">my name is sunny raj</span>
+
+      <header>
+        <span data-testid="this_is_the_heading_section">Heading Present</span>
+      </header>
       <form action="">
         <label htmlFor="name">what is your name </label>
         <input
           type="text"
           name="name"
           id="name"
+          value="Sunny"
           placeholder="Enter your name"
           required
         />
--- src/components/in/in.test.js
+++ src/components/in/in.test.js
@@ -7,20 +7,73 @@ describe("should check the element of Input in In file", () => {
     render(<Input />);
     expect(screen.getByRole("textbox")).toBeInTheDocument();
   });
-  test("should check the input eleemnt in different way", () => {
-    render(<Input />);
-    expect(
-      screen.getByRole("textbox", {
-        name: "what is your name ",
-      })
-    ).toBeInTheDocument();
-  });
+
   test("should check the button element", () => {
     render(<Input />);
     expect(
       screen.getByRole("button", {
         name: "Submit",
       })
+    ).toBeInTheDocument();
+  });
+  test("should check the input element with there placeholder", () => {
+    render(<Input />);
+    expect(screen.getByPlaceholderText("Enter your name")).toBeInTheDocument();
+  });
+  test("should check the heading", () => {
+    render(<Input />);
+    expect(
+      screen.getByRole("heading", {
+        name: "Main Form",
+      })
+    ).toBeInTheDocument();
+  });
+  test("should check the another element", () => {
+    render(<Input />);
+    expect(
+      screen.getByRole("heading", {
+        name: "Section 1",
+      })
+    ).toBeInTheDocument();
+  });
+  test("should check the heading in different way", () => {
+    render(<Input />);
+    expect(
+      screen.getByRole("heading", {
+        level: 1,
+      })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", {
+        level: 2,
+      })
+    ).toBeInTheDocument();
+  });
+  test("should check hading element by getByText", () => {
+    render(<Input />);
+    expect(screen.getByText("Main Form")).toBeInTheDocument();
+    expect(screen.getByText("Section 1")).toBeInTheDocument();
+  });
+  //getbyDesplayValue
+  //---- get by display value can chcek the presence of input by it value
+  test("should check the input element by display value", () => {
+    render(<Input />);
+    expect(screen.getByDisplayValue("Sunny")).toBeInTheDocument();
+  });
+
+  //getByTitle
+  //---- get by element thorugh title
+  // This technique is to write the unit test with title just make the title in element and make it more preferable
+  test("should check the title element", () => {
+    render(<Input />);
+    expect(screen.getByTitle("myName")).toBeInTheDocument();
+  });
+  //to test he getByTestId
+  //---- we have to insert the data-testid value in element
+  test("should check the element by getByTestId", () => {
+    render(<Input />);
+    expect(
+      screen.getByTestId("this_is_the_heading_section")
     ).toBeInTheDocument();
   });
 });
